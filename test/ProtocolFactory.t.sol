341395ae9d7fe1748c8ffd1f55baf70c9a2ce50c
Mauricio Z. Filho <contact@thinkincoin.com>
2025-12-28 18:44:37 -0300

    Remove unused parameters from GovernanceERC20 instantiation in ProtocolFactoryTest

--- test/ProtocolFactory.t.sol
+++ test/ProtocolFactory.t.sol
@@ -1894,9 +1894,7 @@ contract ProtocolFactoryTest is AragonTest {
         });
         address newTokenVotingSetup = address(
             new TokenVotingSetup(
-                new GovernanceERC20(
-                    IDAO(address(0)), "", "", GovernanceERC20.MintSettings(new address[](0), new uint256[](0), true)
-                ),
+                new GovernanceERC20(),
                 new GovernanceWrappedERC20(IERC20Upgradeable(address(0)), "", "")
             )
         );
